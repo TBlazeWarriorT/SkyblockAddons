72a4b4e04110db82fd28831415d9e810b50e3c8d
Brian <speed.bran@hotmail.com>
2020-04-03 23:21:18 -0300

    added transformer my brian hurts
    unfinished

---
+++ src/main/java/codes/biscuit/skyblockaddons/asm/ImageBufferDownloadTransformer.java
@@ -0,0 +1,33 @@
+package codes.biscuit.skyblockaddons.asm;
+
+import codes.biscuit.skyblockaddons.asm.utils.TransformerClass;
+import codes.biscuit.skyblockaddons.asm.utils.TransformerMethod;
+import codes.biscuit.skyblockaddons.tweaker.transformer.ITransformer;
+import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.tree.*;
+
+public class ImageBufferDownloadTransformer implements ITransformer {
+
+    /**
+     * {@link net.minecraft.client.renderer.ImageBufferDownload}
+     */
+    @Override
+    public String[] getClassName() {
+        return new String[]{TransformerClass.ImageBufferDownload.getTransformerName()};
+    }
+
+    @Override
+    public void transform(ClassNode classNode, String name) {
+        for (MethodNode methodNode : classNode.methods) {
+
+            // Objective:
+            // Find: .
+            // Replacement: .
+
+            if (TransformerMethod./**/.matches(methodNode)) {
+                methodNode.instructions.insertBefore(methodNode.instructions.getFirst(), insertConfirmation());
+                break;
+            }
+        }
+    }
+}
